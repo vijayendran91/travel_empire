b89abcc618f38ac6de2c38d85efb8b9d6806bb2d
vijayendran91 <vijayendran91@gmail.com>
2021-10-09 23:16:36 +0530

    Form 1 - Box, From to and Type of Trips

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,8 +4,8 @@ const webpack = require('webpack')
 environment.plugins.prepend(
     'Provide',
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
+      $: 'jquery/src/jquery',
+      jQuery: 'jquery/src/jquery',
       jquery: 'jquery',
       'window.jQuery': 'jquery',
       Popper: ['popper.js', 'default'],
