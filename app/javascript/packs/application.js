5fc2555732bd23e5678d3e14311a7914a1f35dca
vijayendran91 <vijayendran91@gmail.com>
2021-10-22 17:26:20 +0530

    Made changes to fix selector issue in heroku mobile view

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,12 +2,14 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require bootstrap
 //= require jquery3
+//= require jquery-ujs
+//= require jquery-ui
 //= require popper
 //= require bootstrap-sprockets
 //= require jquery3
 //= require popper
-//= require bootstrap
 require("jquery");
 require("@rails/ujs").start();
 require("turbolinks").start();
