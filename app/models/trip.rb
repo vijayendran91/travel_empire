1515d59bc669da3fe747a0d17edc8354a378f9c1
vijayendran91 <vijayendran91@gmail.com>
2021-11-16 15:25:08 +0530

    Added all fields to the trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,19 @@
 class Trip
   include Mongoid::Document
 
+  field :perbus, type: String
+  field :tot, type: String
+  field :adult, type: String
+  field :chldrn, type: String
+  field :pul, type: String
+  field :pua, type: String
+  field :put, type: DateTime
+  field :drl, type: String
+  field :dra, type: String
+  field :drt, type: DateTime
+  field :tob, type: String
   field :fname, type: String
   field :lname, type: String
+  field :phone, type: String
+  field :email, type: String
 end
