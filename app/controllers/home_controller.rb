3d702426fb91cc4d5867ac7daee8b16d50c40ca4
vijayendran91 <vijayendran91@gmail.com>
2021-11-18 17:38:49 +0530

    Successfully completed integrating server code to html.erb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,9 @@ class HomeController < ApplicationController
   end
 
   def submit_trip
+    permitted_param=params[:trip].permit(:perbus, :tot, :adult, :chldrn, :pul, :pua, :put, :drl,:dra,:drt, :fname, :lname, :phone, :email, :gst)
+    @trip = Trip.new(permitted_param)
+    @trip.save
     redirect_to root_path
   end
 end
