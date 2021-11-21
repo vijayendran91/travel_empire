cf29c21d46d6863362c12c7f01fe4acc165a3932
vijayendran91 <vijayendran91@gmail.com>
2021-11-21 18:11:23 +0530

    created initial modal and initial modal for OW-tot

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,6 @@
 //= require bootstrap/dropdown
 //= require bootstrap-select
 //= require bootstrap
-require("jquery");
 require("@rails/ujs").start();
 require("turbolinks").start();
 // require("@rails/activestorage").start();
@@ -21,7 +20,9 @@ require("channels");
 
 import "bootstrap";
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
+import JQuery from 'jquery';
+window.$ = window.JQuery = JQuery;
+import 'bootstrap'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
