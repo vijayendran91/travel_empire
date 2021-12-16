db0302914fa337277e8d99f8834711accb2e8c59
vijayendran91 <vijayendran91@gmail.com>
2021-12-16 17:25:13 +0530

    Added JS Validations on clicking book a ride

--- app/javascript/packs/all_value_validations.js
+++ app/javascript/packs/all_value_validations.js
@@ -1,25 +1,39 @@
 
 function validate_form(){
   var totr=$('#inlineFormCustomSelect').val()
-  
-  // Choose Type of Trip    
+  var todist=$('#fromDistSelect').val();
+  var fromdist = $('#toDistSelect').val();
+  // Choose Type of Trip
   if(totr == 'ch'){
     alert('Choose a Type of Trip');
   }
-  
-  //Cannot start tomorrow and arrivew today - time validation  
+
+  //Cannot start tomorrow and arrivew today - time validation
   if(!(totr == 'ch' || totr == 'ow')){
     st = new Date($('#BookingTime').val());
     end = new Date($('#returnBookingTime').val());
     if((end - st) < 0){
       alert('Return time is before start time.');
     }
-  } 
+  }
+
+  if(totr == 'ch'){
+    alert('Choose a Type of Trip');
+  }
+
+  if(todist == 'sd' || fromdist == 'sd'){
+    alert("Please select a proper district");
+  }
+
+  if($("#aadhaarFront").val() == ""){
+    alert("Please Upload Photo Proof");
+  }
+
 }
 
 
 $(document).ready(function(){
   $('#bookRide').on('click', function(){
     validate_form();
-  })  
+  })
 });
