5f4c9ba8a79f11cffa6a39efa5ec51e65125d6a0
vijayendran91 <vijayendran91@gmail.com>
2022-02-28 19:57:14 +0530

    Checking environment variables 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -135,10 +135,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: 'travel-empire-proofs',
-      access_key_id: 'AKIAVFMMQQYAZKBPNU2W',
-      secret_access_key: '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',
-      s3_region: 'ap-south-1',
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
     }
   }
 
