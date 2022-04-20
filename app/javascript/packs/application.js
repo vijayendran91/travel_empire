4d25daf6cc63627b0231458a6cc005d03beac3ac
vijayendran91 <vijayendran91@gmail.com>
2022-04-20 15:21:45 +0530

    bs 10

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,10 +21,12 @@ import "bootstrap";
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import JQuery from 'jquery';
 window.$ = window.JQuery = JQuery;
-import 'bootstrap'
+// import 'bootstrap'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "bootstrap"
+// import "../../assets/stylesheets/application"
