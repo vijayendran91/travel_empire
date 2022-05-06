2cee4114e3eb2f6fa02c116a73b70587cd4aa0c5
vijayendran91 <vijayendran91@gmail.com>
2022-05-06 22:26:16 +0530

    Removed Phone number validations

--- app/javascript/packs/all_value_validations.js
+++ app/javascript/packs/all_value_validations.js
@@ -35,7 +35,7 @@ function validate_form(){
     alert("Please select a proper district");
     event.preventDefault;
   }
-  validate_phone($('#phone').val())
+  // validate_phone($('#phone').val())
   // if($("#aadhaarFront").val() == ""){
   //   alert("Please Upload Photo Proof");
   // }
@@ -44,24 +44,24 @@ function validate_form(){
 
 
 function validate_phone(phone){
-  result = true;
-  if(phone.length != 10)
-  {
-    result = false;
-  }
-
-  if(result == false){
-    // $('#errorsModal').modal('show');
-    window.alert("Please enter a valid 10 digit Phone number");
-    event.preventDefault;
-  }
+  // result = true;
+  // if(phone.length != 10)
+  // {
+  //   result = false;
+  // }
+  //
+  // if(result == false){
+  //   // $('#errorsModal').modal('show');
+  //   window.alert("Please enter a valid 10 digit Phone number");
+  //   event.preventDefault;
+  // }
 }
 
 $(document).ready(function(){
   $('#bookRide').on('click', function(){
     validate_form();
   })
-  $('#phone').on('change',function(event){
-    validate_phone($('#phone').val());
-  })
+  // $('#phone').on('change',function(event){
+  //   validate_phone($('#phone').val());
+  // })
 });
