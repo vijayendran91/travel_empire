ef76cc81796c26e37e3c306438bdc5d4600141a2
vijayendran91 <vijayendran91@gmail.com>
2022-06-14 16:24:32 +0530

    Added Error Modal and Preview Modal

--- app/javascript/packs/all_value_validations.js
+++ app/javascript/packs/all_value_validations.js
@@ -1,3 +1,4 @@
+import {loadPreviewTripTable} from './trip/preview_modal';
 
 function validate_form(){
   var totr=$('#inlineFormCustomSelect').val()
@@ -5,23 +6,28 @@ function validate_form(){
   var fromdist = $('#toDistSelect').val();
   var str = $('#str').val();
   var error_msgs = [];
+  var msg = '';
   // Choose Type of Trip
-
-  if(totr == 'ch'){
-    msg = 'Choose a Type of Trip';
-    error_msgs.push(msg);
-  }
 
   if(str == 'ch'){
     msg = 'Choose a type of car';
     error_msgs.push(msg);
   }
 
+  if($('#phone').val() == ''){
+    msg = "Please enter a valid phone number";
+    error_msgs.push(msg);
+  }
+
+  if($('#firstName').val() == ''){
+    msg = "Please enter passenger's first name";
+    error_msgs.push(msg);
+  }
   //Cannot start tomorrow and arrivew today - time validation
   if (totr != 'hy'){
     if(!(totr == 'ch' || totr == 'ow')){
-      st = new Date($('#BookingTime').val());
-      end = new Date($('#returnBookingTime').val());
+      var st = new Date($('#BookingTime').val());
+      var end = new Date($('#returnBookingTime').val());
       if((end - st) < 0){
         msg = "Return time is before start time";
         error_msgs.push(msg);
@@ -38,6 +44,7 @@ function validate_form(){
     msg = "Please select a proper district";
     error_msgs.push(msg);
   }
+
   return error_msgs
 }
 
@@ -49,17 +56,18 @@ function fillErrorsInModal(error_msgs){
 }
 
 
-
 $(document).ready(function(){
   $('#bookRide').on('click', function(){
-    debugger;
-    error_msgs = validate_form();
+    var error_msgs = validate_form();
     if(error_msgs.length > 0){
-      debugger;
       fillErrorsInModal(error_msgs);
       $('#errorModal').show();
       window.scrollTo(0, 0);
       event.preventDefault();
+    } else{
+      $('#previeBookingModal').show();
+      window.scrollTo(0, 0);
+      loadPreviewTripTable();
     }
   })
 });
